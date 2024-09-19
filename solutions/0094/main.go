077cd9702f029ab7459b78c7616a9c3dab6afb25
heyuze <ryzehe@gmail.com>
2024-09-19 16:11:12 +0800

    Update solution for problem 0094

--- solutions/0094/main.go
+++ solutions/0094/main.go
@@ -5,7 +5,57 @@ import (
 	"math"
 )
 
+// Bellman-ford 队列优化
 func main() {
+	var n, m int
+	fmt.Scan(&n, &m)
+
+	grid := make([][][]int, n+1)
+	for i := 0; i < m; i++ {
+		var s, t, v int
+		fmt.Scan(&s, &t, &v)
+		grid[s] = append(grid[s], []int{t, v})
+	}
+
+	start := 1
+	end := n
+	queue := []int{1}
+	inTheQueue := make([]bool, n+1)
+	inTheQueue[start] = true
+	minDists := make([]int, n+1)
+	for i := 0; i <= n; i++ {
+		minDists[i] = math.MaxInt64
+	}
+	minDists[start] = 0
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		inTheQueue[node] = false
+
+		for _, edge := range grid[node] {
+			from := node
+			to := edge[0]
+			val := edge[1]
+			if minDists[to] > minDists[from]+val {
+				minDists[to] = minDists[from] + val
+				if !inTheQueue[to] {
+					queue = append(queue, to)
+					inTheQueue[to] = true
+				}
+			}
+		}
+	}
+
+	if minDists[end] != math.MaxInt64 {
+		fmt.Println(minDists[end])
+	} else {
+		fmt.Println("unconnected")
+	}
+}
+
+// Bellman-ford
+func main2() {
 	var n, m int
 	fmt.Scan(&n, &m)
 
